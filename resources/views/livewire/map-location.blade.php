aa7e9697fb8f59e03fc435fbf2179b843f84c110
Tilis Tiadi <tiliztiadi@gmail.com>
2021-01-28 03:25:07 +0800

    add longtitude dan latittude

--- resources/views/livewire/map-location.blade.php
+++ resources/views/livewire/map-location.blade.php
@@ -17,7 +17,20 @@
 					Form
 				</div>
 				<div class="card-body">
-					
+					<div class="row">
+						<div class="col-sm-6">
+							<div class="form-group">
+								<label for="">Longtitude</label>
+								<input wire:model="long" type="text" class="form-control">
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="form-group">
+								<label for="">Latitude</label>
+								<input wire:model="lat" type="text" class="form-control">
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -26,27 +39,32 @@
 
 @section('js')
 <script>
-	const defaultLocation = [122.51865058501699, -3.9863784815205605];
+	{{-- load dulu livewire baru jalankan scriptnya --}}
+	document.addEventListener('livewire:load', () => {
+		const defaultLocation = [122.51865058501699, -3.9863784815205605];
 
-  	mapboxgl.accessToken = '{{ env("MAPBOX_KEY") }}';
-  	var map = new mapboxgl.Map({
-    	container: 'map',
-    	center: defaultLocation,
-    	zoom: 11.15
-  	});
+	  	mapboxgl.accessToken = '{{ env("MAPBOX_KEY") }}';
+	  	var map = new mapboxgl.Map({
+	    	container: 'map',
+	    	center: defaultLocation,
+	    	zoom: 11.15
+	  	});
 
-  	const style = "dark-v10";
-  	
-  	// custom style
-  	map.setStyle(`mapbox://styles/mapbox/${style}`);
-  	// tambah tombol zoom out dan zoom in
-  	map.addControl(new mapboxgl.NavigationControl());
+	  	const style = "dark-v10";
+	  	
+	  	// custom style
+	  	map.setStyle(`mapbox://styles/mapbox/${style}`);
+	  	// tambah tombol zoom out dan zoom in
+	  	map.addControl(new mapboxgl.NavigationControl());
 
-  	map.on('click', (e) => {
-  		const longtitude = e.lngLat.lng;
-  		const lattitude = e.lngLat.lat;
+	  	map.on('click', (e) => {
+	  		const longtitude = e.lngLat.lng;
+	  		const lattitude = e.lngLat.lat;
 
-  		console.table({longtitude, lattitude})
-  	});
+	  		// kirim data ke properti long di MapLocation.php
+	  		@this.long = longtitude;
+	  		@this.lat = lattitude;
+	  	});
+	});
 </script>
 @endsection
